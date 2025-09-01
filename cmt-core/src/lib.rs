a76aef25a1a98d0635ff77ed2ac7fb4d42b1b422
raj <rajeshhjhamain@gmail.com>
2025-09-01 23:06:11 +0530

    added remove

--- cmt-core/src/lib.rs
+++ cmt-core/src/lib.rs
@@ -2,18 +2,15 @@ use crate::utils::calculate_merkle_hash;
 
 mod utils;
 
-pub type Key = Vec<u8>;
-pub type Priority = u128;
+pub type Key = [u8; 32];
+pub type Priority = i128;
 pub type Hash = Vec<u8>;
-
-impl PartialOrd for Key {
-    fn gt(&self, other: &Self) -> bool {}
-}
 
 pub trait Hasher {
     fn hash(data: &[u8]) -> Hash;
 }
 
+#[derive(Debug, Clone)]
 pub struct TreeNode<K, V> {
     pub key: K,
     pub priority: Priority,
@@ -28,68 +25,179 @@ pub struct CartesianMerkleTree<K, V> {
 }
 
 impl<K, V> CartesianMerkleTree<K, V> {
-    pub fn new(root: Option<Box<TreeNode<K, V>>>) -> Self {
-        Self { root }
+    pub fn new() -> Self {
+        Self { root: None }
     }
 
-    pub fn insert(&mut self, key: K, value: V) {
-        // first insert = root
-        if self.root.is_none() {
+    pub fn insert(&mut self, key: K, value: V)
+    where
+        K: PartialOrd + Ord + AsRef<[u8]> + Clone,
+        V: Clone,
+    {
+        let priority = find_priority(&key);
+        self.root = Self::insert_recursive(self.root.take(), key, value, priority);
+    }
+
+    fn insert_recursive(
+        node: Option<Box<TreeNode<K, V>>>,
+        key: K,
+        value: V,
+        priority: Priority,
+    ) -> Option<Box<TreeNode<K, V>>>
+    where
+        K: PartialOrd + Ord + AsRef<[u8]> + Clone,
+        V: Clone,
+    {
+        let mut current_node = match node {
+            Some(n) => n,
+            None => {
+                let hash = calculate_merkle_hash(&key, &Vec::new(), &Vec::new());
+                return Some(Box::new(TreeNode {
+                    key,
+                    priority,
+                    value,
+                    hash,
+                    left: None,
+                    right: None,
+                }));
+            }
+        };
+
+        if priority > current_node.priority {
             let hash = calculate_merkle_hash(&key, &Vec::new(), &Vec::new());
-            let priority = find_priority(&key);
-            self.root = Some(Box::new(TreeNode {
+            let mut new_node = Box::new(TreeNode {
                 key,
                 priority,
                 value,
                 hash,
                 left: None,
                 right: None,
-            }));
-            return;
+            });
+            Self::split(
+                &mut current_node,
+                &new_node.key,
+                &mut new_node.left,
+                &mut new_node.right,
+            );
+            // recompute hash for new_node
+            let left_hash = new_node
+                .left
+                .as_ref()
+                .map(|n| n.hash.clone())
+                .unwrap_or_default();
+            let right_hash = new_node
+                .right
+                .as_ref()
+                .map(|n| n.hash.clone())
+                .unwrap_or_default();
+            new_node.hash = calculate_merkle_hash(&new_node.key, &left_hash, &right_hash);
+            return Some(new_node);
         }
 
-        // walk down the tree
-        let mut node = self.root.as_mut().unwrap();
-        loop {
-            if key < node.key {
-                if let Some(left) = node.left.as_mut() {
-                    node = left;
-                } else {
-                    let hash = calculate_merkle_hash(&key, &Vec::new(), &Vec::new());
-                    let priority = find_priority(&key);
-                    node.left = Some(Box::new(TreeNode {
-                        key,
-                        priority,
-                        value,
-                        hash,
-                        left: None,
-                        right: None,
-                    }));
-                    // TODO: check priority vs node.priority, rotate if needed
-                    break;
-                }
-            } else if key > node.key {
-                if let Some(right) = node.right.as_mut() {
-                    node = right;
-                } else {
-                    let hash = calculate_merkle_hash(&key, &Vec::new(), &Vec::new());
-                    let priority = find_priority(&key);
-                    node.right = Some(Box::new(TreeNode {
-                        key,
-                        priority,
-                        value,
-                        hash,
-                        left: None,
-                        right: None,
-                    }));
-                    // TODO: check priority vs node.priority, rotate if needed
-                    break;
-                }
-            } else {
-                // key == node.key → update value
-                node.value = value;
-                break;
+        if key < current_node.key {
+            current_node.left =
+                Self::insert_recursive(current_node.left.take(), key, value, priority);
+        } else if key > current_node.key {
+            current_node.right =
+                Self::insert_recursive(current_node.right.take(), key, value, priority);
+        } else {
+            current_node.value = value;
+        }
+
+        let left_hash = current_node
+            .left
+            .as_ref()
+            .map(|n| n.hash.clone())
+            .unwrap_or_default();
+        let right_hash = current_node
+            .right
+            .as_ref()
+            .map(|n| n.hash.clone())
+            .unwrap_or_default();
+        current_node.hash = calculate_merkle_hash(&current_node.key, &left_hash, &right_hash);
+
+        Some(current_node)
+    }
+
+    fn split(
+        node: &mut Box<TreeNode<K, V>>,
+        key: &K,
+        left: &mut Option<Box<TreeNode<K, V>>>,
+        right: &mut Option<Box<TreeNode<K, V>>>,
+    ) where
+        K: PartialOrd + Ord + AsRef<[u8]> + Clone,
+        V: Clone,
+    {
+        if node.key < *key {
+            *left = Some(node.clone());
+            if let Some(right_child) = node.right.as_mut() {
+                Self::split(right_child, key, left, right);
             }
+        } else {
+            *right = Some(node.clone());
+            if let Some(left_child) = node.left.as_mut() {
+                Self::split(left_child, key, left, right);
+            }
+        }
+    }
+    pub fn remove(&mut self, key: &K)
+    where
+        K: PartialOrd + Ord + AsRef<[u8]>,
+    {
+        self.root = Self::remove_recursive(self.root.take(), key);
+    }
+
+    fn remove_recursive(node: Option<Box<TreeNode<K, V>>>, key: &K) -> Option<Box<TreeNode<K, V>>>
+    where
+        K: PartialOrd + Ord + AsRef<[u8]>,
+    {
+        if let Some(mut current_node) = node {
+            if *key < current_node.key {
+                current_node.left = Self::remove_recursive(current_node.left.take(), key);
+            } else if *key > current_node.key {
+                current_node.right = Self::remove_recursive(current_node.right.take(), key);
+            } else {
+                // Node found, set priority to -inf and heapify down
+                current_node.priority = i128::MIN;
+                return Self::heapify(current_node);
+            }
+            // Update hash
+            let left_hash = current_node
+                .left
+                .as_ref()
+                .map(|n| n.hash.clone())
+                .unwrap_or_default();
+            let right_hash = current_node
+                .right
+                .as_ref()
+                .map(|n| n.hash.clone())
+                .unwrap_or_default();
+            current_node.hash = calculate_merkle_hash(&current_node.key, &left_hash, &right_hash);
+            return Some(current_node);
+        }
+        None
+    }
+
+    fn heapify(node: Box<TreeNode<K, V>>) -> Option<Box<TreeNode<K, V>>>
+    where
+        K: PartialOrd + Ord + AsRef<[u8]>,
+    {
+        if node.left.is_none() && node.right.is_none() {
+            // Leaf node, remove it
+            return None;
+        }
+
+        let left_priority = node.left.as_ref().map_or(i128::MIN, |n| n.priority);
+        let right_priority = node.right.as_ref().map_or(i128::MIN, |n| n.priority);
+
+        if left_priority > right_priority {
+            let mut new_node = utils::rotate_right(node);
+            new_node.right = Self::heapify(new_node.right.take().unwrap());
+            Some(new_node)
+        } else {
+            let mut new_node = utils::rotate_left(node);
+            new_node.left = Self::heapify(new_node.left.take().unwrap());
+            Some(new_node)
         }
     }
 }
@@ -106,5 +214,5 @@ fn find_priority<K: AsRef<[u8]>>(key: &K) -> Priority {
     let digest = Sha256::digest(key.as_ref());
     let mut bytes = [0u8; 16];
     bytes.copy_from_slice(&digest[..16]);
-    u128::from_be_bytes(bytes)
+    u128::from_be_bytes(bytes) as i128
 }
